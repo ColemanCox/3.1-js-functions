53bb84729f603b6f577e2c1ab2062061b4719a0d
Coleman Cox <colezane82@gmail.com>
2015-07-21 22:46:57 -0500

    trying to put it all together..havn trouble

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,7 +4,7 @@
  */
 
 function arrayEquals(arr1, arr2) {
-    return JSON.stringify(arr1) == JSON.stringify(arr2);
+  return JSON.stringify(arr1) == JSON.stringify(arr2);
 }
 
 /**
@@ -14,75 +14,88 @@ function arrayEquals(arr1, arr2) {
  * arguments and computes the sum of those two numbers.
  */
 
-function sum(a, b){
+function sum(a, b) {
+  for (i = 0; i < a + b; i++) {
+    if (i === sum) {
+      console.log(i);
+    }
+
+  }
+
+  console.assert(sum(8, 11) === 19);
+  console.assert(sum(4, 100) === 104);
+
+  /**
+   * PART 1
+   *
+   * Write a function that calculates the sum of all the numbers in an array
+   */
+
+  function sumOfArray(arr) {
+
     // YOUR CODE HERE
-  return a+b;}
 
-console.assert(sum(8, 11) === 19);
-console.assert(sum(4, 100) === 104);
+  }
 
-/**
- * PART 1
- *
- * Write a function that calculates the sum of all the numbers in an array
- */
+  console.assert(sumOfArray([1, 2]) === 3);
+  console.assert(sumOfArray([]) === 0);
+  console.assert(sumOfArray([1, 2, 3]) === 6);
+  console.assert(sumOfArray([10, 9, 8]) === 27);
 
-function sumOfArray(arr){
-    var sum = 0;
+  /**
+   * PART 2
+   *
+   * write a function that finds the Greatest Common Denominator of two numbers
+   * - if no GCD exists, return 1
+   *
+   * You should use the denominators function from today's class
+   */
+
+  function GCD(a, b) {
+
     // YOUR CODE HERE
-    return sum;
-}
+  }
 
-console.assert(sumOfArray([1, 2]) === 3);
-console.assert(sumOfArray([]) === 0);
-console.assert(sumOfArray([1, 2, 3]) === 6);
-console.assert(sumOfArray([10, 9, 8]) === 27);
+  console.assert(GCD(5, 1) === 1);
+  console.assert(GCD(15, 3) === 3);
+  console.assert(GCD(15, 5) === 5);
+  console.assert(GCD(50, 20) === 10);
 
-/**
- * PART 2
- *
- * write a function that finds the Greatest Common Denominator of two numbers
- * - if no GCD exists, return 1
- *
- * You should use the denominators function from today's class
- */
+  /**
+   * PART 3
+   *
+   * write a function that prints out the Least Common Multiple of two numbers
+   */
 
-function GCD(a, b){
-    // YOUR CODE HERE
-}
+  function LCM(a, b) {
 
-console.assert(GCD(5,1) === 1);
-console.assert(GCD(15,3) === 3);
-console.assert(GCD(15,5) === 5);
-console.assert(GCD(50,20) === 10);
+  }
 
-/**
- * PART 3
- *
- * write a function that prints out the Least Common Multiple of two numbers
- */
+  console.assert(LCM(10, 10) === 10);
+  console.assert(LCM(2, 5) === 10);
+  console.assert(LCM(3, 6) === 6);
+  console.assert(LCM(0, 1) === 1);
 
-function LCM(a, b){
-    // YOUR CODE HERE
-}
+  /**
+   * Part 4
+   *
+   * write a function the returns a FizzBuzz string for some number N (counting up from 1).
+   * - for every number that isn't a multiple of 3 or 5, return a period "."
+   * - for every number that is a multiple of 3 (but not 5), return "fizz"
+   * - for every number that is a multiple of 5 (but not 3), return "buzz"
+   * - for every number that is a multiple of 3 and 5, return "fizzbuzz"
+   */
 
-console.assert(LCM(10,10) === 10)
-console.assert(LCM(2,5) === 10)
-console.assert(LCM(3,6) === 6)
-console.assert(LCM(0,1) === 1)
+  function fizzbuzz(N) {
+    debugger;
+    for (var N = 1; i < 100; i++) {
+      if (n % 3 != 0 || i % 5 != 0)
+          console.log(".");
 
-/**
- * Part 4
- *
- * write a function the returns a FizzBuzz string for some number N (counting up from 1).
- * - for every number that isn't a multiple of 3 or 5, return a period "."
- * - for every number that is a multiple of 3 (but not 5), return "fizz"
- * - for every number that is a multiple of 5 (but not 3), return "buzz"
- * - for every number that is a multiple of 3 and 5, return "fizzbuzz"
- */
+    }
+  }
 
-function fizzbuzz(N){
-    // YOUR CODE HERE
+  // YOUR CODE HERE
 }
 
 console.assert(fizzbuzz(1) === ".")
